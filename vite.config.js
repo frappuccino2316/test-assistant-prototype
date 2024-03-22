f8ccf5252bfb6ef8076ee56135eab9b41fdc33b2
frappuccino2316 <frappuccino2316@gmail.com>
2024-03-23 02:03:22 +0900

    first

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig((opt) => {
+  return {
+    root: 'src',
+    build: {
+      outDir: '../dist',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/content_script.ts'),
+        },
+        output: {
+          entryFileNames: 'content_script.js',
+        },
+      },
+    },
+  };
+});
