bf8727e23b075359a65f279fd8ca2c4933556bcb
frappuccino2316 <frappuccino2316@gmail.com>
2024-04-01 23:44:34 +0900

    途中

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,7 +1,28 @@
-const domain = location.href.split('/')[2];
+const url = location.href.split('?')[0];
+const body = document.body;
 
 (async () => {
-  const response = await chrome.runtime.sendMessage({ currentDomain: domain });
-  // ↓にtrueのときの処理を追加する
+  console.log(body);
+  const response = await chrome.runtime.sendMessage({ url: url, body: body });
   console.log(response);
+  if (response.domainExists) {
+    console.log('domainが設定済み');
+    // ↓にtrueのときの処理を追加する
+    console.log(response.status);
+    switch (response.status) {
+      case 'added':
+        console.log(response.newBody);
+        break;
+      case 'failure':
+        console.log(response.newBody);
+        break;
+      case 'comparing':
+        console.log(response.previousBody);
+        console.log(response.newBody);
+        break;
+      default:
+        console.log('not root');
+        break;
+    }
+  }
 })();
--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,13 +1,35 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  includeDomain(request.currentDomain).then((result) => {
-    console.log(result);
-    sendResponse({ domainExists: result });
+  const domain = request.url.split('/')[2];
+  const body = request.body;
+  const url = request.url;
+
+  console.log(body);
+
+  includeDomain(domain).then((result) => {
+    getDom(url).then((dom) => {
+      if (JSON.stringify(dom) === JSON.stringify({})) {
+        addDom(body, url).then((res) => {
+          sendResponse({ domainExists: result, status: res, newBody: body });
+        });
+      } else {
+        // console.log(dom);
+        // console.log(body);
+        // console.log(dom.isEqualNode(body));
+        sendResponse({
+          domainExists: result,
+          status: 'comparing',
+          previousBody: dom,
+          newBody: body,
+        });
+      }
+      sendResponse({ domainExists: result });
+    });
   });
-  return true;
 });
 
 // TODO
 // 以下は他ファイルからimportしたらエラーになってしまうので、一旦ベタ書きしている
+// 問題を解決したらファイル分離したい
 export const includeDomain = async (currentDomain: string): Promise<boolean> => {
   const configuredDomains: string[] = await getConfiguredDmains();
   if (currentDomain === undefined) {
@@ -23,4 +45,29 @@ export const getConfiguredDmains = async (): Promise<string[]> => {
   const storage = await chrome.storage.local.get('configuredDomains');
   const configuredDomains: string[] = storage.configuredDomains ? storage.configuredDomains : [];
   return configuredDomains;
+};
+
+// dom情報を持っておきたい
+export const addDom = async (body: object, url: string) => {
+  console.log('↓addDom');
+  console.log(body);
+  console.log(url);
+  if (body) {
+    await chrome.storage.local.set({ url: body });
+    return 'added';
+  } else {
+    return 'failure';
+  }
+};
+
+export const removeDom = async (url: string) => {
+  await chrome.storage.local.remove(url, () => {
+    console.log('successfully deteled');
+  });
+};
+
+export const getDom = async (url: string): Promise<object> => {
+  const storage = await chrome.storage.local.get(url);
+  const dom = storage.url ? storage.url : {};
+  return dom;
 };
